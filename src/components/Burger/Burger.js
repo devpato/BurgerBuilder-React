e50c940ca8b72ae1df1d563513336f06caf9cc0a
devpato <pevargas2730@gmail.com>
2020-03-30 02:02:47 -0400

    checkin if there are no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,19 @@ import React from "react";
 import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = ({ ingredients }) => {
-  const transformedIngredients = Object.keys(ingredients).map(key => {
+  let transformedIngredients = Object.keys(ingredients).map(key => {
     return [...Array(ingredients[key])].map((_, i) => {
       return <BurgerIngredient key={key + i} type={key} />;
     });
   });
-  console.log(transformedIngredients);
+
+  transformedIngredients =
+    transformedIngredients.flat(1).length === 0 ? (
+      <p>Please add ingredients</p>
+    ) : (
+      transformedIngredients
+    );
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
