4fda774391017e8b3e71659d7821726a314cb69d
devpato <pevargas2730@gmail.com>
2020-03-30 02:14:58 -0400

    created reusable buil control

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./BuildControl.module.css";
+
+const buildControl = ({ label }) => {
+  return (
+    <div className={styles.BuildControl}>
+      <div className={styles.Label}>{label}</div>
+      <button className={styles.Less}>Less</button>
+      <button className={styles.More}>More</button>
+    </div>
+  );
+};
+
+export default buildControl;
