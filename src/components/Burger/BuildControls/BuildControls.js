b831c949aadb5b0a9618416f59314c9ef4b08abc
devpato <pevargas2730@gmail.com>
2020-03-30 23:34:21 -0400

    showing price of the burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,8 +9,16 @@ const controls = [
   { label: "Meat", type: "meat" }
 ];
 
-const buildControls = ({ ingredientAdded, ingredientRemoved, disabled }) => (
+const buildControls = ({
+  ingredientAdded,
+  ingredientRemoved,
+  disabled,
+  price
+}) => (
   <div className={styles.BuildControls}>
+    <p>
+      Current Price: <strong>{price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
