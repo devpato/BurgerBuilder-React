d6c5ff1b803d87d264c857d97d0fe4c24d7cced0
devpato <pevargas2730@gmail.com>
2020-03-30 23:32:19 -0400

    removed items working

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./BuildControl.module.css";
 
-const buildControl = ({ label, added }) => {
+const buildControl = ({ label, added, removed, disabled }) => {
   return (
     <div className={styles.BuildControl}>
       <div className={styles.Label}>{label}</div>
-      <button className={styles.Less}>Less</button>
+      <button className={styles.Less} onClick={removed} disabled={disabled}>
+        Less
+      </button>
       <button className={styles.More} onClick={added}>
         More
       </button>
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,13 +9,15 @@ const controls = [
   { label: "Meat", type: "meat" }
 ];
 
-const buildControls = ({ ingredientAdded }) => (
+const buildControls = ({ ingredientAdded, ingredientRemoved, disabled }) => (
   <div className={styles.BuildControls}>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => ingredientAdded(ctrl.type)}
+        removed={() => ingredientRemoved(ctrl.type)}
+        disabled={disabled[ctrl.type]}
       />
     ))}
   </div>
