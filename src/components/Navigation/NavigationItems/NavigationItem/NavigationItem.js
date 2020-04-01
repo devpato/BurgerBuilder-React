544f28c0e9ad626cde194a4d60ce308e66282a80
devpato <pevargas2730@gmail.com>
2020-04-01 00:03:39 -0400

    Navigation items and item

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NavigationItem.module.css";
+
+const navigationItem = ({ children, link, active }) => (
+  <li className={styles.NavigationItem}>
+    <a href={link} className={active ? styles.active : null}>
+      {children}
+    </a>
+  </li>
+);
+
+export default navigationItem;
