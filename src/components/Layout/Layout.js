475de700c76705d6c21f85536dcd511e695247c3
devpato <pevargas2730@gmail.com>
2020-04-01 00:45:44 -0400

    close functionality sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import styles from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = ({ children }) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={styles.Content}>{children}</main>
-  </Aux>
-);
+class layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,24 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import styles from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
-const sideDrawer = () => {
+const sideDrawer = ({ open, closed }) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
   return (
-    <div className={styles.SideDrawer}>
-      <Logo logoHeight="12%" />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show={open} clicked={closed} />
+      <div className={attachedClasses.join(" ")}>
+        <Logo logoHeight="12%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
